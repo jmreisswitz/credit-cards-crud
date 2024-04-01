d52d7074e415958d197c57512d691d32d9b29938
Jose Mario Reisswitz <jmreisswitz@gmail.com>
2024-03-31 23:16:41 -0300

    feat: spring initializr

---
+++ src/test/java/com/jmreisswitz/creditcards/CreditCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmreisswitz.creditcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

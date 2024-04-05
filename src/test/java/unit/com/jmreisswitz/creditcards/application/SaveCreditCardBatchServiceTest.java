4779dba8350f65ea9f5ca4bacfc70276410195db
Jose Mario Reisswitz <jmreisswitz@gmail.com>
2024-04-05 02:40:37 -0300

    fix: tests

--- src/test/java/unit/com/jmreisswitz/creditcards/application/SaveCreditCardBatchServiceTest.java
+++ src/test/java/unit/com/jmreisswitz/creditcards/application/SaveCreditCardBatchServiceTest.java
@@ -4,6 +4,7 @@ import com.jmreisswitz.creditcards.application.SaveCreditCardBatchService;
 import com.jmreisswitz.creditcards.domain.creditcard.batch.CreditCardBatch;
 import com.jmreisswitz.creditcards.domain.creditcard.batch.CreditCardBatchRepository;
 import com.jmreisswitz.creditcards.domain.creditcard.validator.CreditCardNumberValidator;
+import com.jmreisswitz.creditcards.infrastructure.security.CreditCardDataEncoder;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
@@ -13,8 +14,9 @@ class SaveCreditCardBatchServiceTest {
 
     CreditCardBatchRepository creditCardBatchRepository = mock(CreditCardBatchRepository.class);
     CreditCardNumberValidator creditCardNumberValidator = mock(CreditCardNumberValidator.class);
+    CreditCardDataEncoder creditCardDataEncoder = mock(CreditCardDataEncoder.class);
     SaveCreditCardBatchService saveCreditCardBatchService =
-            new SaveCreditCardBatchService(creditCardBatchRepository, creditCardNumberValidator);
+            new SaveCreditCardBatchService(creditCardBatchRepository, creditCardNumberValidator, creditCardDataEncoder);
 
     @Test
     void delegateToRepositoryAfterValidating() {
